eda45fae610078a0798a0162accaf1ee9c06b0d0
Kylie Harris <geekykylie@gmail.com>
2025-11-03 12:05:30 -0800

    Finished Triangle.java

--- src/shapes/Triangle.java
+++ src/shapes/Triangle.java
@@ -1,5 +1,28 @@
 package shapes;
 
 public class Triangle extends Shape {
-    private double
+    private double a;
+    private double b;
+    private double a;
+    private double gamma;
+
+    public Triangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+        this.gamma = 90;
+    }
+
+    public Triangle(double a, double b, double gamma) {
+        this.a = a;
+        this.b = b;
+        this.gamma = gamma;
+    }
+
+    public double getArea() {
+        return a * b * Math.sin(Math.toRadians(gamma)) / 2;
+    }
+
+    public String getName() {
+        return "Triangle";
+    }
 }
