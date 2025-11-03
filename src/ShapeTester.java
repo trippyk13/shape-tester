835f5627694bc519c7e0eae1461a98486919b18f
Kylie Harris <geekykylie@gmail.com>
2025-11-03 12:54:09 -0800

    Setup ShapeTester.java

--- src/ShapeTester.java
+++ src/ShapeTester.java
@@ -1,2 +1,17 @@
+import shapes.*;
+import java.util.*;
+
 public class ShapeTester {
+    public static void main(String[] args) {
+        ArrayList<Shape> shapes = new ArrayList<>();
+
+        shapes.add(ShapeFactory.createShape("circle", 5));
+        shapes.add(ShapeFactory.createShape("rectangle", 5, 2));
+        shapes.add(ShapeFactory.createShape("triangle", 5, 2));
+        shapes.add(ShapeFactory.createShape("triangle", 5, 2, 50));
+
+        for (Shape s : shapes) {
+            System.out.println(s.getName() + " area: " + s.getArea());
+        }
+    }
 }
