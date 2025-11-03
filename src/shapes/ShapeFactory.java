73bf89d60fe3b1162a44d80a389575e8343c1e53
Kylie Harris <geekykylie@gmail.com>
2025-11-03 12:34:41 -0800

    Setup ShapeFactory.java

---
+++ src/shapes/ShapeFactory.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class ShapeFactory {
+    public static Shape createShape(String type, double... params) {
+        switch (type.toLowerCase()) {
+            case "circle":
+                return new Circle(params[0]);
+            case "rectangle":
+                return new Rectangle(params[0], params[1]);
+            case  "triangle":
+                return new Triangle(params[0], params[1], params[2]);
+            default:
+                return null;
+        }
+    }
+}
